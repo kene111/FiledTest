16617e9f9553f6f9aebd036af0758af40a9db8e2
kenechi Ojukwu <kenechiojukwu@gmail.com>
2021-02-26 02:06:07 +0100

    added deployment files

--- audio_api.py
+++ audio_api.py
@@ -1,19 +1,26 @@
+import os
 from flask import Flask
 from database.db import initialize_db
 from flask_restful import Api
 from resources.routes import initialize_routes
 from resources.errors import errors
-
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+
 api = Api(app, errors=errors)
 
-app.config.from_envvar('ENV_FILE_LOCATION')
+#used for development
+#app.config.from_envvar('ENV_FILE_LOCATION')
 
-#app.config['MONGODB_SETTINGS'] = {
-#    'host': 'mongodb://localhost/audiodb'
-#}
+
+MONGODB_URI = os.environ.get('MONGODB_URI')
+
+app.config['MONGODB_SETTINGS'] = {
+    'host': MONGODB_URI
+}
 
 
 initialize_db(app)
